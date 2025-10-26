b4d573c169709bd970840f3516bed1c20cad87f3
Aniruddha Mandal <ani96dh@gmail.com>
2025-10-26 20:13:50 +0530

    .

--- src/ugs_sampler/__init__.pyi
+++ src/ugs_sampler/__init__.pyi
@@ -1,5 +1,5 @@
 import torch
-
+from typing import Tuple
 def create_preproc(edge_index, num_nodes: int, k: int) -> int:
     """Builds Preprocessing 
     """
@@ -12,7 +12,7 @@ def sample_batch(edge_index: torch.Tensor,
                  ptr: torch.Tensor,
                  m_per_graph: int,
                  k: int,
-                 mode: str = "sample"):
+                 mode: str = "sample")->Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]: 
     """
     Sample a fixed number of subgraphs from each graph in a batched graph dataset.
 
@@ -32,30 +32,25 @@ def sample_batch(edge_index: torch.Tensor,
                 - "sample": indices are local to each subgraph (0..k-1)
                 - "graph":  indices are local to the parent graph
                 - "global": indices are global within the batch
-    Returns:
+    Returns
         nodes_t (torch.Tensor):
             Tensor of shape [B_total, k], where B_total = num_graphs * m_per_graph. 
             Each row lists the global node IDs of the sampled subgraph (padded with -1).
-
         edge_index_t (torch.Tensor):
             Tensor of shape [2, E_total_sampled], concatenating edges from all sampled subgraphs. 
             Node indices are represented according to the chosen `mode`.
-
         edge_ptr_t (torch.Tensor):
             Tensor of shape [B_total + 1], CSR-style pointer array indicating the start
             and end positions in `edge_index_t` for each sampled subgraph.
-
         sample_ptr_t (torch.Tensor):
             Tensor of shape [num_graphs + 1], pointer array mapping subgraphs back to
             their parent graphs.
-
         edge_src_global_t (torch.Tensor):
             Tensor of shape [E_total_sampled], giving the original edge column indices
             in the input `edge_index` corresponding to each sampled edge. This can be used
             to gather edge attributes for the subgraphs.
-    
     Notes:
         - Edge attributes can be aligned via:
-              sub_edge_attr = edge_attr[edge_src_global_t]
+            sub_edge_attr = edge_attr[edge_src_global_t]
     """
     ...
